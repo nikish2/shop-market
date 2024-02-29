ad512c9abf63bf236a27129753c3bd009f50d7fb
nikish2 <rashkevich2001@mail.ru>
2024-02-29 20:32:51 +0300

    все кроме футера

--- app/js/main.js
+++ app/js/main.js
@@ -55,3 +55,26 @@ let swiper = new Swiper(".mySwiper", {
     },
   },
 });
+
+let swiperTwo = new Swiper(".mySwiper-two", {
+  slidesPerView: 1,
+  spaceBetween: 10,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  breakpoints: {
+    640: {
+      slidesPerView: 1,
+      spaceBetween: 20,
+    },
+    768: {
+      slidesPerView: 2,
+      spaceBetween: 30,
+    },
+    1024: {
+      slidesPerView: 3,
+      spaceBetween: 40,
+    },
+  },
+});
